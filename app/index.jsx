8883ce615c89cb037cc0400ea26f3170cc55a76d
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-11-04 20:10:29 +0600

    merge

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,25 @@
+// import { Redirect } from "expo-router"
+// import { useEffect, useLayoutEffect, useState } from "react"
+// import * as SecureStore from "expo-secure-store"
+
+// const index = () => {
+//     return <Redirect href={"(about)/antiscam"} />
+// }
+
+// export default index
+
 import { Redirect } from "expo-router"
 import { useEffect, useLayoutEffect, useState } from "react"
 import * as SecureStore from "expo-secure-store"
 
 const index = () => {
-    return <Redirect href={"(about)/antiscam"} />
+    const [state, setState] = useState(null)
+    useLayoutEffect(() => {
+        const token = SecureStore.getItem("userToken")
+        console.log(token)
+        setState(token)
+    }, [])
+    return <Redirect href={state ? "(root)/home" : "(auth)/welcome"} />
 }
 
 export default index
